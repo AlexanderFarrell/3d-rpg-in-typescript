30ef77ad05b998c4cb66c97f10cc15dcc08828a7
AlexanderFarrell <courses@alexanderfarrell.com>
2026-04-24 13:14:21 +0100

    Half way

--- engine/physics/physics.ts
+++ engine/physics/physics.ts
@@ -1,20 +1,24 @@
+import { vec3 } from "gl-matrix";
 import type { Location } from "../components/location";
 import type { Array2D } from "../util/array2d";
 import { Component } from "../world/entity";
 
-export interface Sphere {
+export class Sphere {
 	radius: number;
+
+	public constructor(radius: number) {
+		this.radius = radius;
+	}
 }
 
-export interface Box {
-	halfWidth: number;
-	halfHeight: number;
-	halfDepth: number;
-}
-
-export interface Cylinder {
+export class Cylinder {
 	radius: number;
 	height: number;
+
+	public constructor(radius: number, height: number) {
+		this.radius = radius;
+		this.height = height;
+	}
 }
 
 export type Shape = Sphere | Cylinder;
@@ -29,7 +33,8 @@ export class PhysicalObj extends Component {
 	}
 
 	onStart(): void {
-		this.location = 
+		if (!this.entity!.has())
+		this.location = this.entity!.get
 	}
 }
 
@@ -44,7 +49,45 @@ export class Physics {
 			}
 		}
 		else if (a.shape instanceof Cylinder) {
-			if (b.shape instanceof )
+			if (b.shape instanceof Sphere) {
+
+			}
+			else if (b.shape instanceof Cylinder) {
+
+			}
 		}
 	}
+}
+
+function DoesSphereIntersectSphere(
+	a: Sphere,
+	b: Sphere,
+	aLoc: Location,
+	bLoc: Location,
+) {
+	// Get distance
+	let distance = vec3.distance(aLoc.position, bLoc.position);
+	return distance < (a.radius + b.radius);
+}
+
+function DoesCylinderIntersectCylinder(
+	a: Cylinder,
+	b: Cylinder,
+	aLoc: Location,
+	bLoc: Location
+) {
+	//Ensure the heights are within range
+	let aHighY = a.height/2 + aLoc.position[1];
+	let bHighY = b.height/2 + bLoc.position[1];
+	let aLowY = aHighY + a.height/2;
+	let bLowY = bHighY + b.height/2;
+	if (aLoc.position[1] > bHighY ||
+		aLoc.position[1] < bLowY ||
+		bLoc.position[1] > aHighY ||
+		bLoc.position[1] < aLowY
+	) {
+		
+	} 
+
+	return false;
 }
